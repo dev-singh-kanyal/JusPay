10e06a84b4e8b5eba567fac1cb5f41a558e8d95a
Dev Singh Kanyal <77870205+dev-singh-kanyal@users.noreply.github.com>
2023-12-14 18:59:22 +0530

    Updated with last submitted code

--- tree_of_space.cpp
+++ tree_of_space.cpp
@@ -6,27 +6,27 @@ using namespace std;
 
 struct Node
 {
-    string label;
-    vector<Node *> children;
-    Node *parent;
-    int ancestorLocked, descendantLocked, userID;
-    bool isLocked;
+  string label;
+  vector<Node *> children;
+  Node *parent;
+  int ancestorLocked, descendantLocked, userID;
+  bool isLocked;
 
-    Node(string name, Node *parentNode)
-    {
-        label = name;
-        parent = parentNode;
-        ancestorLocked = descendantLocked = userID = 0;
-        isLocked = false;
-    }
+  Node(string name, Node *parentNode)
+  {
+    label = name;
+    parent = parentNode;
+    ancestorLocked = descendantLocked = userID = 0;
+    isLocked = false;
+  }
 
-    void addChildren(vector<string> childLabels, Node *parentNode)
+  void addChildren(vector<string> childLabels, Node *parentNode)
+  {
+    for (auto &childLabel : childLabels)
     {
-        for (auto &childLabel : childLabels)
-        {
-            children.push_back(new Node(childLabel, parentNode));
-        }
+      children.push_back(new Node(childLabel, parentNode));
     }
+  }
 };
 
 // Print the Tree
@@ -47,235 +47,253 @@ struct Node
 //     }
 // }
 
-Node *buildTree(Node *root, int &numChildren, vector<string> &nodeLabels)
-{
-    queue<Node *> q;
-    q.push(root);
-
-    int startIndex = 1;
-    while (!q.empty())
-    {
-        Node *currentNode = q.front();
-        q.pop();
-
-        if (startIndex >= nodeLabels.size())
-        {
-            continue;
-        }
-
-        vector<string> tempChildrenLabels;
-        for (int i = startIndex; i < startIndex + numChildren; i++)
-        {
-            tempChildrenLabels.push_back(nodeLabels[i]);
-        }
-
-        currentNode->addChildren(tempChildrenLabels, currentNode);
-        startIndex += numChildren;
-
-        for (auto child : currentNode->children)
-        {
-            q.push(child);
-        }
-    }
-
-    return root;
-}
+Node *buildTree(Node *root, int &numChildren, vector<string> &nodeLabels);
 
 class LockingTree
 {
 private:
-    Node *root;
-    unordered_map<string, Node *> labelToNode;
-    vector<string> outputLog;
+  Node *root;
+  unordered_map<string, Node *> labelToNode;
+  vector<string> outputLog;
 
 public:
-    LockingTree(Node *treeRoot) { root = treeRoot; }
+  LockingTree(Node *treeRoot) { root = treeRoot; }
+  Node *getRoot() { return root; }
 
-    Node *getRoot() { return root; }
+  void fillLabelToNode(Node *currentNode)
+  {
+    if (!currentNode)
+      return;
+    labelToNode[currentNode->label] = currentNode;
+    for (auto child : currentNode->children)
+      fillLabelToNode(child);
+  }
 
-    void fillLabelToNode(Node *currentNode)
+  void updateDescendant(Node *currentNode, int value)
+  {
+    for (auto child : currentNode->children)
     {
-        if (!currentNode)
-        {
-            return;
-        }
-        labelToNode[currentNode->label] = currentNode;
-        for (auto child : currentNode->children)
-        {
-            fillLabelToNode(child);
-        }
+      child->ancestorLocked += value;
+      updateDescendant(child, value);
+    }
+  }
+
+  bool checkDescendantsLocked(Node *currentNode, int &id,
+                              vector<Node *> &lockedNodes)
+  {
+    if (currentNode->isLocked)
+    {
+      if (currentNode->userID != id)
+        return false;
+      lockedNodes.push_back(currentNode);
     }
 
-    void updateDescendants(Node *currentNode, int value)
+    if (currentNode->descendantLocked == 0)
+      return true;
+
+    bool result = true;
+
+    for (auto child : currentNode->children)
     {
-        for (auto child : currentNode->children)
-        {
-            child->ancestorLocked += value;
-            updateDescendants(child, value);
-        }
+      result &= checkDescendantsLocked(child, id, lockedNodes);
+      if (!result)
+        return false;
     }
 
-    bool checkDescendantsLocked(Node *currentNode, int &id, vector<Node *> &lockedNodes)
-    {
-        if (currentNode->isLocked)
-        {
-            if (currentNode->userID != id)
-                return false;
-            lockedNodes.push_back(currentNode);
-        }
-        if (currentNode->descendantLocked == 0)
-            return true;
+    return result;
+  }
 
-        bool result = true;
-        for (auto child : currentNode->children)
-        {
-            result &= checkDescendantsLocked(child, id, lockedNodes);
-            if (!result)
-                return false;
-        }
-        return result;
+  bool lockNode(string label, int id)
+  {
+    Node *targetNode = labelToNode[label];
+
+    if (targetNode->isLocked)
+      return false;
+
+    if (targetNode->ancestorLocked != 0 || targetNode->descendantLocked != 0)
+      return false;
+
+    Node *currentNode = targetNode->parent;
+
+    while (currentNode)
+    {
+      currentNode->descendantLocked++;
+      currentNode = currentNode->parent;
     }
 
-    bool lockNode(string label, int id)
+    updateDescendant(targetNode, 1);
+    targetNode->isLocked = true;
+    targetNode->userID = id;
+
+    return true;
+  }
+
+  bool unlockNode(string label, int id)
+  {
+    Node *targetNode = labelToNode[label];
+
+    if (!targetNode->isLocked)
+      return false;
+
+    if (targetNode->isLocked && targetNode->userID != id)
+      return false;
+
+    Node *currentNode = targetNode->parent;
+
+    while (currentNode)
     {
-        Node *targetNode = labelToNode[label];
-        if (targetNode->isLocked)
-            return false;
-
-        if (targetNode->ancestorLocked != 0 || targetNode->descendantLocked != 0)
-            return false;
-
-        Node *currentNode = targetNode->parent;
-        while (currentNode)
-        {
-            currentNode->descendantLocked++;
-            currentNode = currentNode->parent;
-        }
-        updateDescendants(targetNode, 1);
-        targetNode->isLocked = true;
-        targetNode->userID = id;
-        return true;
+      currentNode->descendantLocked--;
+      currentNode = currentNode->parent;
     }
 
-    bool unlockNode(string label, int id)
+    updateDescendant(targetNode, -1);
+    targetNode->isLocked = false;
+
+    return true;
+  }
+
+  bool upgradeNode(string label, int id)
+  {
+    Node *targetNode = labelToNode[label];
+
+    if (targetNode->isLocked)
+      return false;
+
+    if (targetNode->ancestorLocked != 0 || targetNode->descendantLocked == 0)
+      return false;
+
+    vector<Node *> lockedDescendants;
+
+    if (checkDescendantsLocked(targetNode, id, lockedDescendants))
     {
-        Node *targetNode = labelToNode[label];
-        if (!targetNode->isLocked)
-            return false;
-        if (targetNode->isLocked && targetNode->userID != id)
-            return false;
-
-        Node *currentNode = targetNode->parent;
-        while (currentNode)
-        {
-            currentNode->descendantLocked--;
-            currentNode = currentNode->parent;
-        }
-        updateDescendants(targetNode, -1);
-        targetNode->isLocked = false;
-        return true;
+      for (auto lockedDescendant : lockedDescendants)
+      {
+        unlockNode(lockedDescendant->label, id);
+      }
     }
+    else
+      return false;
 
-    bool upgradeNode(string label, int id)
+    lockNode(label, id);
+
+    return true;
+  }
+
+  void processQueries(vector<pair<int, pair<string, int>>> queries)
+  {
+    for (auto query : queries)
     {
-        Node *targetNode = labelToNode[label];
-        if (targetNode->isLocked)
-            return false;
+      int opcode = query.first;
+      string nodeLabel = query.second.first;
+      int userId = query.second.second;
 
-        if (targetNode->ancestorLocked != 0 || targetNode->descendantLocked == 0)
-            return false;
-
-        vector<Node *> lockedDescendants;
-        if (checkDescendantsLocked(targetNode, id, lockedDescendants))
-        {
-            for (auto lockedDescendant : lockedDescendants)
-            {
-                unlockNode(lockedDescendant->label, id);
-            }
-        }
-        else
-            return false;
-
-        lockNode(label, id);
-        return true;
+      switch (opcode)
+      {
+      case 1:
+        lockNode(nodeLabel, userId) ? outputLog.push_back("true")
+                                    : outputLog.push_back("false");
+        break;
+      case 2:
+        unlockNode(nodeLabel, userId) ? outputLog.push_back("true")
+                                      : outputLog.push_back("false");
+        break;
+      case 3:
+        upgradeNode(nodeLabel, userId) ? outputLog.push_back("true")
+                                       : outputLog.push_back("false");
+        break;
+      }
     }
+  }
 
-    void processQueries(vector<pair<int, pair<string, int>>> queries)
+  void printOutputLog()
+  {
+    for (const string &result : outputLog)
     {
-        for (auto query : queries)
-        {
-            int opcode = query.first;
-            string nodeLabel = query.second.first;
-            int userID = query.second.second;
-
-            switch (opcode)
-            {
-            case 1:
-                if (lockNode(nodeLabel, userID))
-                {
-                    outputLog.push_back("true");
-                }
-                else
-                {
-                    outputLog.push_back("false");
-                }
-                break;
-            case 2:
-                if (unlockNode(nodeLabel, userID))
-                {
-                    outputLog.push_back("true");
-                }
-                else
-                {
-                    outputLog.push_back("false");
-                }
-                break;
-            case 3:
-                if (upgradeNode(nodeLabel, userID))
-                {
-                    outputLog.push_back("true");
-                }
-                else
-                {
-                    outputLog.push_back("false");
-                }
-                break;
-            }
-        }
+      cout << result << "\n";
     }
-
-    void printOutputLog()
-    {
-        for (const string &result : outputLog)
-        {
-            cout << result << "\n";
-        }
-    }
+  }
 };
 
 int main()
 {
-    int numNodes, numChildren, numQueries;
-    cin >> numNodes >> numChildren >> numQueries;
+  int numNodes, numChildren, numQueries;
+  cin >> numNodes >> numChildren >> numQueries;
 
-    vector<string> nodeLabels(numNodes);
-    for (int i = 0; i < numNodes; i++)
-        cin >> nodeLabels[i];
+  vector<string> nodeLabels(numNodes);
 
-    Node *rootNode = new Node(nodeLabels[0], nullptr);
-    rootNode = buildTree(rootNode, numChildren, nodeLabels);
+  for (int i = 0; i < numNodes; i++)
+    cin >> nodeLabels[i];
 
-    LockingTree lockingTree(rootNode);
-    lockingTree.fillLabelToNode(lockingTree.getRoot());
+  Node *rootNode = new Node(nodeLabels[0], nullptr);
+  rootNode = buildTree(rootNode, numChildren, nodeLabels);
 
-    vector<pair<int, pair<string, int>>> queries(numQueries);
-    for (int i = 0; i < numQueries; i++)
-    {
-        cin >> queries[i].first >> queries[i].second.first >> queries[i].second.second;
-    }
+  LockingTree lockingTree(rootNode);
+  lockingTree.fillLabelToNode(lockingTree.getRoot());
 
-    lockingTree.processQueries(queries);
-    lockingTree.printOutputLog();
+  vector<pair<int, pair<string, int>>> queries(numQueries);
 
-    return 0;
+  for (int i = 0; i < numQueries; i++)
+  {
+    cin >> queries[i].first >> queries[i].second.first >>
+        queries[i].second.second;
+  }
+
+  lockingTree.processQueries(queries);
+  lockingTree.printOutputLog();
+  return 0;
 }
+
+// build the tree
+Node *buildTree(Node *root, int &numChildren, vector<string> &nodeLabels)
+{
+  queue<Node *> q;
+  q.push(root);
+
+  int startIndex = 1;
+
+  while (!q.empty())
+  {
+    Node *currentNode = q.front();
+    q.pop();
+
+    if (startIndex >= nodeLabels.size())
+      continue;
+
+    vector<string> tempChildrenLabels;
+
+    for (int i = startIndex; i < startIndex + numChildren; i++)
+      tempChildrenLabels.push_back(nodeLabels[i]);
+
+    currentNode->addChildren(tempChildrenLabels, currentNode);
+    startIndex += numChildren;
+
+    for (auto child : currentNode->children)
+      q.push(child);
+  }
+
+  return root;
+}
+
+// Input
+// 7
+// 2
+// 5
+// World
+// Asia
+// Africa
+// China
+// India
+// SouthAfrica
+// Egypt
+// 1 China 9
+// 1 India 9
+// 3 Asia 9
+// 2 India 9
+// 2 Asia 9
+
+// Output
+// true
+// true
+// true
+// false
+// true
